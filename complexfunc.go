41919544a97a0d6502bb587f97c7b1badd436cf6
Makoto Naruse <mako_marimo@yahoo.co.jp>
2020-09-03 18:36:24 +0900

    fix for vet

--- complexfunc.go
+++ complexfunc.go
@@ -1,7 +1,6 @@
 package complexfunc
 
 import (
-	"fmt"
 	"go/ast"
 	"go/token"
 	"golang.org/x/tools/go/analysis"
@@ -25,9 +24,9 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	fmt.Println("Calculated by AST Search")
+	//fmt.Println("Calculated by AST Search")
 	calcByAST(pass)
-	fmt.Println("Calculated by SSA and Control Graph")
+	//fmt.Println("Calculated by SSA and Control Graph")
 	calcBySSA(pass)
 	return nil, nil
 }
@@ -35,9 +34,9 @@ func run(pass *analysis.Pass) (interface{}, error) {
 func calcBySSA(pass *analysis.Pass) (interface{}, error) {
 	s := pass.ResultOf[buildssa.Analyzer].(*buildssa.SSA)
 	for _, f := range s.SrcFuncs {
-		fmt.Println("func name:", f.Name())
+		//fmt.Println("func name:", f.Name())
 		complex := complexity(f)
-		fmt.Println("complex:", complex)
+		//fmt.Println("complex:", complex)
 		if complex > 10 {
 			pass.Reportf(f.Pos(), "function %s is too complicated %d > 10", f.Name(), complex)
 		}
@@ -58,7 +57,7 @@ func complexity(fn *ssa.Function) int {
 		edges += len(b.Succs)
 	}
 	nodes := len(fn.Blocks)
-	fmt.Println("n:", nodes, "e:", edges)
+	//fmt.Println("n:", nodes, "e:", edges)
 	return edges - nodes + 2
 }
 
@@ -72,10 +71,10 @@ func calcByAST(pass *analysis.Pass) (interface{}, error) {
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		switch n := n.(type) {
 		case *ast.FuncDecl:
-			fmt.Println("func name:", n.Name)
+			//fmt.Println("func name:", n.Name)
 			complex := 1
 			complex += calcComplex(n)
-			fmt.Println("complex", complex)
+			//fmt.Println("complex", complex)
 		}
 		num := 10
 		switch num {
